ffe6d81ef95319bfc6ebdf2987e50be22601144a
harineko0 <harineko0927@gmail.com>
2024-07-11 21:14:45 +0900

    fix gha

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,17 +1,11 @@
-import { NextRequest, NextResponse } from "next/server";
-import {Admin} from "@/server/firebase-admin";
+import { NextResponse } from 'next/server';
+import { Admin } from '@/server/firebase-admin';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
     try {
         const users = await Admin.auth.listUsers(10);
-        console.log(users);
-        return NextResponse.json({users: users});
+        return NextResponse.json({ users: users });
     } catch (e) {
-        console.error(e);
-        return NextResponse.json({error: "ERROR"}, {status: 500});
+        return NextResponse.json({ error: 'ERROR' }, { status: 500 });
     }
-}
-
-export function POST(request: NextRequest): NextResponse {
-    return new NextResponse();
 }
--- src/server/firebase-admin.ts
+++ src/server/firebase-admin.ts
@@ -1,9 +1,14 @@
 import * as admin from 'firebase-admin';
 
 // 二重初期化を防止
-const app = admin.apps.length === 1 ? admin : admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(atob(process.env.FIRE_SA_KEY))),
-});
+const app =
+    admin.apps.length === 1
+        ? admin
+        : admin.initializeApp({
+              credential: admin.credential.cert(
+                  JSON.parse(atob(process.env.FIRE_SA_KEY))
+              ),
+          });
 
 export namespace Admin {
     export const auth = app.auth();
