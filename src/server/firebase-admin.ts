6277c8e7336edfded39fd7d2c45a956fc3b932b4
harineko0 <harineko0927@gmail.com>
2024-07-11 21:00:44 +0900

    added *.rules

--- src/server/firebase-admin.ts
+++ src/server/firebase-admin.ts
@@ -2,20 +2,7 @@ import * as admin from 'firebase-admin';
 
 // 二重初期化を防止
 const app = admin.apps.length === 1 ? admin : admin.initializeApp({
-    credential: admin.credential.cert({
-        type: process.env.FIRE_SA_TYPE,
-        project_id: process.env.FIRE_PROJECT_ID,
-        private_key_id: process.env.FIRE_SA_PRIVATE_KEY_ID,
-        // .env は \n が \\n になるので置換する
-        private_key: process.env.FIRE_SA_PRIVATE_KEY?.replaceAll('\\n', '\n'),
-        client_email: process.env.FIRE_SA_CLIENT_EMAIL,
-        client_id: process.env.FIRE_SA_CLIENT_ID,
-        auth_uri: process.env.FIRE_SA_AUTH_URI,
-        token_uri: process.env.FIRE_SA_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.FIRE_SA_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.FIRE_SA_CLIENT_X509_CERT_URL,
-        universe_domain: process.env.FIRE_SA_UNIVERSE_DOMAIN,
-    } as any),
+    credential: admin.credential.cert(JSON.parse(atob(process.env.FIRE_SA_KEY))),
 });
 
 export namespace Admin {
