b8490266f8ac38c3cc7f7036df8087e411951486
harineko0 <harineko0927@gmail.com>
2024-08-02 18:37:59 +0900

    ADD: add syntax highlighter

--- src/app/posts/[...topicId]/_components/markdown.tsx
+++ src/app/posts/[...topicId]/_components/markdown.tsx
@@ -4,17 +4,21 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import 'katex/dist/katex.min.css';
 import './markdown.css';
+import CodeBlock from "@/app/posts/[...topicId]/_components/codeblock";
 
 interface Props {
     children: string;
 }
 
-export default function Markdown({ children }: Props) {
+export default function Markdown({children}: Props) {
     return (
         <div className={'markdown-body'}>
             <ReactMarkdown
                 remarkPlugins={[remarkGfm, remarkMath]}
                 rehypePlugins={[rehypeKatex]}
+                components={{
+                    pre: CodeBlock
+                }}
             >
                 {children}
             </ReactMarkdown>
