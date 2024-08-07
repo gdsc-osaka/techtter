c375d40e69e7c2beb1c51169583f910f58570338
harineko0 <harineko0927@gmail.com>
2024-08-07 15:30:24 +0900

    fix?

--- src/app/posts/[...topicId]/layout.tsx
+++ src/app/posts/[...topicId]/layout.tsx
@@ -6,14 +6,18 @@ import { ReactNode } from 'react';
 const topicRepository = new AdminTopicRepository();
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const topicId = params.topicId.pop();
-    const topic =
-        topicId === undefined ? undefined : await topicRepository.find(topicId);
-    if (topic === undefined) return { title: 'Topic not found.' };
-
     return {
-        title: topic.name,
+        title: 'Test',
     };
+
+    // const topicId = params.topicId.pop();
+    // const topic =
+    //     topicId === undefined ? undefined : await topicRepository.find(topicId);
+    // if (topic === undefined) return { title: 'Topic not found.' };
+    //
+    // return {
+    //     title: topic.name,
+    // };
 }
 
 export default function Layout({ children }: { children: ReactNode }) {
--- src/app/posts/[...topicId]/page.tsx
+++ src/app/posts/[...topicId]/page.tsx
@@ -1,5 +1,6 @@
 import PostList from '@/app/posts/[...topicId]/_components/postList';
 import { AdminTopicRepository } from '@/infrastructure/topic/adminTopicRepository';
+import { sfetch } from '@/lib/fetchUtils';
 
 export interface Props {
     params: {
@@ -11,15 +12,22 @@ const topicRepository = new AdminTopicRepository();
 
 export default async function PostListPage({ params }: Props) {
     const topicId = params.topicId.pop();
-    const topic =
-        topicId === undefined ? undefined : await topicRepository.find(topicId);
-    if (topic === undefined) return <p>Topic not found.</p>;
-
     return (
         <PostList
-            topicId={topic.id}
-            topicRight={topic.right}
-            topicLeft={topic.left}
+            topicId={topicId  ?? "test"}
+            topicRight={-100000}
+            topicLeft={100000}
         />
     );
+    // const topic =
+    //     topicId === undefined ? undefined : await topicRepository.find(topicId);
+    // if (topic === undefined) return <p>Topic not found.</p>;
+    //
+    // return (
+    //     <PostList
+    //         topicId={topic.id}
+    //         topicRight={topic.right}
+    //         topicLeft={topic.left}
+    //     />
+    // );
 }
