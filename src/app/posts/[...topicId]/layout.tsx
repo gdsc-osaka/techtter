40a7f3b87fcac5e5c6e2706951ba6ab8812c63d8
harineko0 <harineko0927@gmail.com>
2024-08-07 21:17:17 +0900

    fix?

--- src/app/posts/[...topicId]/layout.tsx
+++ src/app/posts/[...topicId]/layout.tsx
@@ -1,19 +1,18 @@
 import { Props } from '@/app/posts/[...topicId]/page';
-import { AdminTopicRepository } from '@/infrastructure/topic/adminTopicRepository';
 import { Metadata } from 'next';
 import { ReactNode } from 'react';
 
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const topicId = params.topicId[params.topicId.length - 1];
-    const topicRepository = new AdminTopicRepository();
-    const topic = await topicRepository.find(topicId);
-    if (topic === undefined) return { title: 'Topic not found.' };
-
-    return {
-        title: topic.name,
-    };
-}
-
 export default function Layout({ children }: { children: ReactNode }) {
     return children;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const topicId = params.topicId[params.topicId.length - 1];
+    // const topicRepository = new AdminTopicRepository();
+    // const topic = await topicRepository.find(topicId);
+    // if (topic === undefined) return { title: 'Topic not found.' };
+
+    return {
+        title: topicId,
+    };
 }
