3570cd76fc28d6251fea0beeff944ac9bbaa1835
harineko0 <harineko0927@gmail.com>
2024-08-07 21:42:12 +0900

    why

--- src/app/posts/[...topicId]/page.tsx
+++ src/app/posts/[...topicId]/page.tsx
@@ -1,3 +1,6 @@
+import PostList from "@/app/posts/[...topicId]/_components/postList";
+import { AdminTopicRepository } from "@/infrastructure/topic/adminTopicRepository";
+
 export interface Props {
     params: {
         topicId: string[];
@@ -5,20 +8,20 @@ export interface Props {
 }
 
 export default async function PostListPage({ params }: Props) {
-    // const topicId = params.topicId[params.topicId.length - 1];
-    // const topicRepository = new AdminTopicRepository();
-    // const topic = await topicRepository.find(topicId);
-    // if (topic === undefined) return <p>Topic not found.</p>;
-
-    return (
-        <div>PostListPage</div>
-    );
+    const topicId = params.topicId[params.topicId.length - 1];
+    const topicRepository = new AdminTopicRepository();
+    const topic = await topicRepository.find(topicId);
+    if (topic === undefined) return <p>Topic not found.</p>;
 
     // return (
-    //     <PostList
-    //         topicId={topicId}
-    //         // topicRight={topic.right}
-    //         // topicLeft={topic.left}
-    //     />
+    //     <div>PostListPage</div>
     // );
+
+    return (
+        <PostList
+            topicId={topicId}
+            // topicRight={topic.right}
+            // topicLeft={topic.left}
+        />
+    );
 }
