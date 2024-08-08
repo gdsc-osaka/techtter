4d1f6b7431f53d9c79bf63a325d881d4a47e9e09
harineko0 <harineko0927@gmail.com>
2024-08-08 12:41:16 +0900

    restore getUser()

--- src/app/api/users/[uid]/route.ts
+++ src/app/api/users/[uid]/route.ts
@@ -1,22 +1,13 @@
 import { FireUser } from '@/domain/fireUser';
+import { auth } from "@/firebaseAdmin";
 import { NextRequest, NextResponse } from 'next/server';
-
-function getUser(uid: string) {
-    return {
-        uid: uid,
-        displayName: 'test',
-        photoURL: 'https://example.com',
-    };
-}
 
 export async function GET(
     req: NextRequest,
     { params }: { params: { uid: string } }
 ) {
     try {
-        // const resultUser = await auth.getUser(params.uid);
-        const resultUser = getUser(params.uid);
-        // logger.log(`Got user ${resultUser.uid}`);
+        const resultUser = await auth.getUser(params.uid);
         const fireUser: FireUser = {
             uid: resultUser.uid,
             displayName: resultUser.displayName || null,
@@ -27,7 +18,6 @@ export async function GET(
             user: fireUser,
         });
     } catch (e) {
-        // logger.error(e);
         return NextResponse.json(
             { error: 'Internal server error' },
             { status: 500 }
