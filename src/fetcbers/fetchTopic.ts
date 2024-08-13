4ce6c6a119bb6b40f71bb3e499da7e300f608e97
harineko0 <harineko0927@gmail.com>
2024-08-13 14:00:03 +0900

    remove console log

--- src/fetcbers/fetchTopic.ts
+++ src/fetcbers/fetchTopic.ts
@@ -4,10 +4,7 @@ import { replacePlainObjWithTimestamp } from '@/lib/timestampUtils';
 
 export default function fetchTopic(topicId: string): Promise<Topic> {
     return sfetch(`/api/topics/${topicId}`, { cache: 'force-cache' })
-        .then((res) => {
-            res.text().then(t => console.log(t))
-            return res.json();
-        })
+        .then((res) => res.json())
         .then((d) => {
             const topic = replacePlainObjWithTimestamp(d.topic);
             assertsTopic(topic);
