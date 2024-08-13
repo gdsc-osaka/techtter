21a5e73831bbb55cbf91ed3aaff3408c3defc683
harineko0 <harineko0927@gmail.com>
2024-08-13 13:52:34 +0900

    add console log

--- src/fetcbers/fetchTopic.ts
+++ src/fetcbers/fetchTopic.ts
@@ -3,8 +3,11 @@ import { sfetch } from '@/lib/fetchUtils';
 import { replacePlainObjWithTimestamp } from '@/lib/timestampUtils';
 
 export default function fetchTopic(topicId: string): Promise<Topic> {
-    return fetch(`https://techtter-git-fix-isr-2-harineko0s-projects.vercel.app/api/topics/${topicId}`, { cache: 'force-cache' })
-        .then((res) => res.json())
+    return sfetch(`/api/topics/${topicId}`, { cache: 'force-cache' })
+        .then((res) => {
+            res.text().then(t => console.log(t))
+            return res.json();
+        })
         .then((d) => {
             const topic = replacePlainObjWithTimestamp(d.topic);
             assertsTopic(topic);
