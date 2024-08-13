e87fdf94579f5176074a615710ae3e046644154b
harineko0 <harineko0927@gmail.com>
2024-08-13 13:41:36 +0900

    removed sfetch?

--- src/fetcbers/fetchTopic.ts
+++ src/fetcbers/fetchTopic.ts
@@ -3,7 +3,7 @@ import { sfetch } from '@/lib/fetchUtils';
 import { replacePlainObjWithTimestamp } from '@/lib/timestampUtils';
 
 export default function fetchTopic(topicId: string): Promise<Topic> {
-    return sfetch(`/api/topics/${topicId}`, { cache: 'force-cache' })
+    return fetch(`https://techtter-git-fix-isr-2-harineko0s-projects.vercel.app/api/topics/${topicId}`, { cache: 'force-cache' })
         .then((res) => res.json())
         .then((d) => {
             const topic = replacePlainObjWithTimestamp(d.topic);
